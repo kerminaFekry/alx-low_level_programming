99ed09271cd5404d8a8418175772038cfe2b5c80
Your Name <you@example.com>
2023-08-11 19:14:33 -0700

    f

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,13 +15,14 @@ srand(time(0));
 n = rand();
 
 printf("Last digit of %d is ", n);
-int lastDigit;                                                                                                                                                          lastDigit = n % 10;
-if (lastDigit > 5)
-printf("and is greater than 5\n");
-else if (lastDigit == 0)
-printf("and is 0\n");
-else
-printf("and is less than 6 and not 0\n");
+int lastDigit; \
+lastDigit = n % 10;
+    if (lastDigit > 5)
+        printf("and is greater than 5\n");
+    else if (lastDigit == 0)
+        printf("and is 0\n");
+    else
+        printf("and is less than 6 and not 0\n");
 
-return (0);
+    return (0);
 }
