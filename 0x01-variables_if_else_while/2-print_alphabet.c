6a8883d0c436b77b9524d781d53c525b81c04888
Your Name <you@example.com>
2023-08-19 23:29:52 -0700

    hhhhhhhhhhhhh

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,11 @@
  */
 int main(void)
 {
-    char letter;
+char letter;
 
-    for (letter = 97; letter <= 122; letter++)
-    {
-        putchar(letter);
-    }
-  
-
-    return (0);
+for (letter = 97; letter <= 122; letter++)
+{
+putchar(letter);
+}
+return (0);
 }
