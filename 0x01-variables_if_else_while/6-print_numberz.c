2e32907b3c21d951b30b0d83382a143ed4a15f4d
Your Name <you@example.com>
2023-08-21 12:41:57 -0700

    hhhhhhhh

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int number;
+
+for (number = 48; number <= 57; number++)
+{
+putchar(number);
+}
+putchar(10);
+return (0);
+}
