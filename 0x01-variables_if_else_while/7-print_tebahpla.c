57d4dd62124ed379c2646add8ea3897194f940d6
Your Name <you@example.com>
2023-08-21 13:09:50 -0700

    hhhhhhhhhhhhhhh

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>\
+/**\
+* main - Entry point of the program
+ *
+ * Return: Always 0
+ */\
+int main(void)
+{
+char letter;
+for (letter = 122; letter >= 97; letter--)\
+{\
+putchar (letter);\
+}\
+putchar(10);\
+return(0);
+}\
