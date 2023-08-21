39ffa08ae03c635b1bac2c5275c5cb3bb4c6340d
Your Name <you@example.com>
2023-08-21 13:48:34 -0700

    jjjjjjjjjj

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int number;
+char letter;
+for (number = 48; number <= 57; number++)
+{
+putchar(number);
+}
+for (letter = 97; letter <= 103; letter++)
+{
+putchar (letter);
+}
+putchar (10);
+return (0);
+} 
