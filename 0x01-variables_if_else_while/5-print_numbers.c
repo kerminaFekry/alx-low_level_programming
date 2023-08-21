b009a42b29537e0fb84b197d2fcb3e068f92229c
Your Name <you@example.com>
2023-08-21 12:33:17 -0700

    hhhhhhhh

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,9 +9,9 @@ int main(void)
 {
 int number;
 
-for (number = 0; number < 11; number++)
+for (number = 0; number < 10; number++)
 {
-putchar(number );
+putchar(int number);
 }
 putchar(10);
 return (0);
