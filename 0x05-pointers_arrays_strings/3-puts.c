bb9714481ad568dd41b1a0cf02374a36e083477d
Your Name <you@example.com>
2023-08-21 18:46:07 -0700

    hhh

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_rev - Prints a string in reverse followed by a new line.
+ * @s: Pointer to a string.
+ */
+void print_rev(char *s)
+{
+int length = _strlen(s);
+
+while (length > 0)
+{
+putchar(s[length - 1]);
+length--;
+}
+
+putchar(10);
+}
