b0d5c2ac3795ab452d2ae82357b9e6a2a2b41ca9
Your Name <you@example.com>
2023-08-21 19:40:07 -0700

    hh

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * print_array - Prints n elements of an array of integers, followed by a new line.
+ * print_array - Prints n elements of an array of integers
  * @a: Pointer to an array of integers.
  * @n: Number of elements to be printed.
  */
