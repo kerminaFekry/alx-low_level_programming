687de0f0738b6657742eb58a40ae55fe108ed80c
Your Name <you@example.com>
2023-08-21 19:45:03 -0700

    ghhh

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,5 @@
 /**
- * _strcpy - Copies the string pointed to by src, including the terminating null byte, to the buffer pointed to by dest.
+ * _strcpy - Copies the string pointed to by src, including
  * @dest: Pointer to the destination buffer.
  * @src: Pointer to the source string.
  *
