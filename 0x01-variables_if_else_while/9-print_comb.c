82467ddcef085d2642450b69b2563af077b920a2
Your Name <you@example.com>
2023-08-25 01:41:57 -0700

    jjjjjjjjjjjjj

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,21 +4,18 @@
  *
  * Return: 0 on success
  */
-int main(void) {
+int main(void) 
+{
 int num1 ;
 for (num1 = 48; num1 < 58; num1++) 
 {
-if (num1 == 57)
-{\
 
 putchar(num1);
-}
-else 
+if (num1 != 57)
 {
-putchar(num1 ) ;
 putchar(32) ;
 putchar(44);
-}\
+}
 }
 putchar(10);
 return (0);
