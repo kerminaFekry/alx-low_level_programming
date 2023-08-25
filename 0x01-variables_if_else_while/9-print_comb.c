01048f004a8f7fbf9d06a58040067586a9fc4bce
Your Name <you@example.com>
2023-08-25 01:29:42 -0700

    hhhhhh

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,25 @@
-#include <stdio.h>
+#include <stdio.h>                                                     
 /**
  * main - Entry point of the program
  *
  * Return: 0 on success
  */
 int main(void) {
-int num1, num2;
-for (num1 = 0; num1 < 10; num1++) {
-for (num2 = 0; num2 < 10; num2++) {
-putchar(num1 + 48);
-putchar(num2 + 48);
-if (num1 != 9 || num2 != 9) {
-putchar(32);
+int num1 ;
+for (num1 = 48; num1 < 58; num1++) 
+{
+if (num1 == 57)
+{\
+
+putchar(num1);
+}
+else 
+{
+putchar(num1 ) ;
+putchar(32) ;
 putchar(44);
+}\
 }
-}
-}
-/**putchar(10);*/
-return 0;
+putchar(10);
+return (0);
 }
