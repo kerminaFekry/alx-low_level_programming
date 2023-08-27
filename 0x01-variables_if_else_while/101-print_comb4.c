7550e186a63aaae4b24ea535b7a10f45c41677dc
Your Name <you@example.com>
2023-08-27 05:20:18 -0700

    jjjj

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    int num1, num2, num3;
+
+    for (num1 = 0; num1 <= 7; num1++)
+    {
+        for (num2 = num1 + 1; num2 <= 8; num2++)
+        {
+            for (num3 = num2 + 1; num3 <= 9; num3++)
+            {
+                putchar(num1 + 0);
+                putchar(num2 + 0);
+                putchar(num3 + 0);
+
+                if (num1 != 7 || num2 != 8 || num3 != 9)  \
+{\
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar(10);
+return(0);
+}
