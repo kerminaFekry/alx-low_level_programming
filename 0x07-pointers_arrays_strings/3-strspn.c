3f924d2cb7edbbce828756477b53641bf11ca991
Your Name <you@example.com>
2023-08-28 07:02:07 -0700

    jjj

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: Pointer to the string to be searched.
+ * @accept: Pointer to the string containing the characters to match.
+ *
+ * Return: The number of bytes in the initial segment of s
+ *         consisting only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int count = 0;
+char *ptr;
+
+while (*s)
+{
+ptr = accept;
+while (*ptr)
+{
+if (*ptr == *s)
+{
+count++;
+break;
+}
+ptr++;
+}
+if (*ptr == 0)
+break;
+s++;
+}
+
+return (count);\
+}
