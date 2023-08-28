8f06c08c0b7b2d0219674367611346cb6b701a98
Your Name <you@example.com>
2023-08-28 07:15:34 -0700

    kkkkkkkkkkkkkkkk

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,4 @@
-/**
+/**              
  * _strchr - Locates the first occurrence of the character c
  *           in the string s.
  * @s: Pointer to the string.
@@ -6,15 +6,16 @@
  *
  * Return: Pointer to the first occurrence of the character c
  *         in the string s, or NULL if not found.
- */ #include <stddef.h>
+ */
+#include <stddef.h>
 char *_strchr(char *s, char c)
-{
+{                     
 while (*s)
 {
 if (*s == c)
 return s;
-s++;
-}
-
+s++;              
+}    
+                          
 return (NULL);
 }
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,12 @@
-/**
+/**                                                                       
  * _strpbrk - Searches a string for any of a set of bytes.
  * @s: Pointer to the string to be searched.
  * @accept: Pointer to the string containing the bytes to match.
  *
  * Return: Pointer to the byte in s that matches one of the
  *         bytes in accept, or NULL if no such byte is found.
- */ #include <stddef.h>
+ */
+#include <stddef.h>
 char *_strpbrk(char *s, char *accept)
 {
 while (*s)
