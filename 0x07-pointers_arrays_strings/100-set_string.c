ae7a0909988a4c36113380bd8715d1a81acc1dbc
Your Name <you@example.com>
2023-08-28 08:25:07 -0700

    kkkkkkkkkkkkkk

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,7 +1,12 @@
 /**
-*ask gbt fot the elaustration
-*/
+ * set_string - Sets the value of a pointer to a char.
+ * @s: Double pointer to a char.
+ * @to: Pointer to a char.
+ *
+ * Description: The function sets the value of a double pointer
+ *              to a char to the address of a pointer to a char.
+ */
 void set_string(char **s, char *to)
 {
-    *s = to;
+*s = to;
 }
