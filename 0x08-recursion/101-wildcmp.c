5db36ca64a9d84c402285517cc7c69e6b50927c3
Your Name <you@example.com>
2023-09-20 22:27:30 -0700

    llllllllllllllll

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * wildcmp - Compares two strings and returns 1 if the strings can be considered identical,
+ *           otherwise returns 0. The second string can contain the special character *,
+ *           which can match any string, including an empty string.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared.
+ *
+ * Return: 1 if the strings are identical, 0 otherwise.
+ */
+int wildcmp(char *s1, char *s2)
+{
+if (*s2 == 42)
+{
+if (*(s2 + 1) == 0)
+return 1;
+
+if (*s1 == 0)
+return wildcmp(s1, s2 + 1);
+
+return wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2);
+}
+
+if (*s1 != *s2)
+return 0;
+
+if (*s1 == 0)
+return 1;
+
+return wildcmp(s1 + 1, s2 + 1);
+
+}
